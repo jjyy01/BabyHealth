5813c535de14dfa66c8d8449fdd1846e23aa49cf
jiwei <weiji@weji.info>
2015-06-02 11:30:48 +0800

    Initial Commit

---
+++ BabyHealth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BabyHealth
+//
+//  Created by jiwei on 15/6/2.
+//  Copyright (c) 2015年 weiji.info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BabyHealth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BabyHealth
+//
+//  Created by jiwei on 15/6/2.
+//  Copyright (c) 2015年 weiji.info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
